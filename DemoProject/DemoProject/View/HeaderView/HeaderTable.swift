be5cbc76646433218526716dd597570d37a0b414
ducndthe153148 <ducndthe153148@fpt.edu.vn>
2022-01-17 17:19:40 +0700

    init

--- DemoProject/DemoProject/View/HeaderView/HeaderTable.swift
+++ DemoProject/DemoProject/View/HeaderView/HeaderTable.swift
@@ -9,25 +9,27 @@ import UIKit
 
 class HeaderTable: UIView {
     
-//    override init(frame: CGRect) {
-//        super.init(frame: frame)
-//        setUpView()
-//    }
-//
-//    required init?(coder: NSCoder) {
-//        super.init(coder: coder)
-//        setUpView()
-//    }
-//
-//    private func setUpView(){
-//        Bundle.main.loadNibNamed("HeaderTable", owner: self, options: nil)
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUpView()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setUpView()
+    }
+
+    private func setUpView(){
+//        let xibView = Bundle.main.loadNibNamed("HeaderTable", owner: self, options: nil) as! UIView
+//        xibView.frame = self.bounds
+//        addSubview(xibView)
 //        self.addSubview(contentView)
 //        contentView.frame = self.bounds
 //        contentView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-//
-////        locationViewModel.sendLocation.subscribe(onNext: { [weak self] result in
-////            print("Dong 36: Result is:")
-////        })
-//    }
+
+//        locationViewModel.sendLocation.subscribe(onNext: { [weak self] result in
+//            print("Dong 36: Result is:")
+//        })
+    }
     
 }
