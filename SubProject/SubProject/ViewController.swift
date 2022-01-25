37d765f25e79c98073e0ae15126cdbfe9eabd9b9
ducndthe153148 <ducndthe153148@fpt.edu.vn>
2022-01-25 15:47:30 +0700

    done

--- SubProject/SubProject/ViewController.swift
+++ SubProject/SubProject/ViewController.swift
@@ -26,45 +26,76 @@ class ViewController: UIViewController {
         
         // Disable scroll of table view
         self.tableView.isScrollEnabled = false
-//        self.tableView.frame.size.height = self.tableView.contentSize.height
         
-//        tableView.estimatedRowHeight = 68.0
+//        tableView.estimatedRowHeight = 120
 //        tableView.rowHeight = UITableView.automaticDimension
         
         //no need to write following if checked in storyboard
-        self.scrollView.bounces = false
-        self.tableView.bounces = true
+//        self.scrollView.bounces = false
+//        self.tableView.bounces = true
                 
         tableView.register(TableViewCell.nib(), forCellReuseIdentifier: TableViewCell.identifier)
     }
     
-//    override func viewDidAppear(_ animated: Bool) {
-//        tableHeightConstraint.constant = tableView.contentSize.height
-//        super.viewDidAppear(animated)
+    // MARK: - NEW - Cái này hoạt động ạ, mà nó dài hẳn lên
+//    override func viewWillAppear(_ animated: Bool) {
+//        self.tableView.addObserver(self, forKeyPath: "contentSize", options: .new, context: nil)
+//        self.tableView.reloadData()
+//    }
+//
+//    override func viewWillDisappear(_ animated: Bool) {
+//        self.tableView.removeObserver(self, forKeyPath: "contentSize")
+//    }
+//
+//    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+//        if keyPath == "contentSize" {
+//            if let newValue = change?[.newKey] {
+//                let newSize = newValue as! CGSize
+//                self.tableHeightConstraint.constant = newSize.height
+//            }
+//            print("running here")
+//        }
 //    }
     
-    // MARK: - View
-    override func updateViewConstraints() {
-        tableHeightConstraint.constant = tableView.contentSize.height
-        self.tableView.layoutIfNeeded()
+    // MARK: - Cái này đíu hoạt động
+//    override func updateViewConstraints() {
+//        self.tableHeightConstraint.constant = CGFloat(self.tableView.contentSize.height)
+//        self.tableView.layoutIfNeeded()
+//        self.view.layoutIfNeeded()
+//        self.scrollView.layoutIfNeeded()
+//        super.updateViewConstraints()
+//    }
+    
+    //gets called anytime your view controller's view has its bounds changed.
+    override func viewWillLayoutSubviews() {
         super.updateViewConstraints()
+        DispatchQueue.main.async {
+            self.tableHeightConstraint.constant = self.tableView.contentSize.height
+        }
     }
     
 }
 
 extension ViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return 10
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: TableViewCell.identifier, for: indexPath) as! TableViewCell
-        cell.txtInfor.text = "開始日時：2021年12月28日12:33:52 開始日時：2021年12月28日12:33:52 開始日時：2021年12月28日12:33:52"
+        cell.txtInfor.text = "Trung Duc dep trai qua di nuihfsdinầ  jadfnfaonfspdfui h hduanòu \(indexPath.row)"
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        //self.viewWillLayoutSubviews()
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return UITableView.automaticDimension
     }
     
+    func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableView.automaticDimension
+    }
 }
